0807bedb62ff17854b1e43005a03c1f01499febd
Tayler Stoppel <stoppel@ksu.edu>
2023-06-06 20:08:13 -0500

    initial commit

---
+++ BBFWebApp/Models/News.cs
@@ -0,0 +1,11 @@
+﻿namespace BBFWebApp.Models
+{
+    public class News
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+    }
+}
